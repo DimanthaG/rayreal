a33f05a33dada90a54c0cbeccb9c8314c35d8889
DimanthaG <theenslaveyer@gmail.com>
2025-01-03 00:12:39 -0500

    Analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'; // Updated for React 18
 import { NextUIProvider } from '@nextui-org/react';
 import App from './App';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Initialize React root
 root.render(
   <React.StrictMode>
     <NextUIProvider>
       <App />
+      <Analytics/>
     </NextUIProvider>
   </React.StrictMode>
 );
