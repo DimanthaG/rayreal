7d3e191a03d82b5fcb38732fc841734d11315d11
DimanthaG <theenslaveyer@gmail.com>
2025-01-04 10:12:00 -0500

    Yes

--- src/components/ContactUsPage/ContactUsPage.js
+++ src/components/ContactUsPage/ContactUsPage.js
@@ -16,11 +16,11 @@ const ContactUsPage = () => {
             <Card.Body>
               <Text h4>Raytronics Group</Text>
               <Text>330/08, Saman Mawatha, Lake Road, Borelesgamuwa</Text>
-              <Text css={{ marginTop: '1rem' }}>Dr. K H Lasantha Gunawardana: 0777727527</Text>
-              <Text>Shamika Chandupa: +94 (72) 664 5529</Text>
-              <Text>Dan Poddiwela: 0(77) 112 5807</Text>
+              <Text css={{ marginTop: '1rem' }}>Dr. K H Lasantha Gunawardana: +94777727527</Text>
+              <Text>Shamika Chandupa: +94726645529</Text>
+              <Text>Dan Poddiwela: +94771125807</Text>
               <Text css={{ marginTop: '1rem' }}>
-                Email: <a href="mailto:lasa_ray@yahoo.com">lasa_ray@yahoo.com</a>
+                Email: <a href="mailto:raytronicspvtltd@gmail.com">raytronicspvtltd@gmail.com</a>
               </Text>
             </Card.Body>
           </Card>
